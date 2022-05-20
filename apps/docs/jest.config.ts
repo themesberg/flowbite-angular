cbc5698c88006a93fe37c47d0a2642f7694fe0cb
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2022-05-20 23:52:31 +0200

    build: update nx to 14.1.7

--- apps/docs/jest.config.ts
+++ apps/docs/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
   displayName: 'docs',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
