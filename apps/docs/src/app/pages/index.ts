bb5bcb6689282b4e77031c761503387cdd8a6b6a
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2022-08-09 15:32:39 +0100

    feat: Added Breadcrumb docs page

--- apps/docs/src/app/pages/index.ts
+++ apps/docs/src/app/pages/index.ts
@@ -3,3 +3,4 @@ export * from './alert-page/alert-page.component';
 export * from './badge-page/badge-page.component';
 export * from './dashboard-page/dashboard-page.component';
 export * from './sidebar-page/sidebar-page.component';
+export * from './breadcrumb-page/breadcrumb-page.component';
