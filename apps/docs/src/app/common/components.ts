ed930ea161878fc94e65ee8a93e874a9af551369
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2022-08-09 22:59:37 +0100

    chore: Add component card

--- apps/docs/src/app/common/components.ts
+++ apps/docs/src/app/common/components.ts
@@ -53,6 +53,10 @@ export const components: RouteProps[] = [
     icon: '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>',
     href: '/breadcrumb',
     group: false,
+    card: {
+      className: 'w-56',
+      images: { light: 'breadcrumb-light.svg', dark: 'breadcrumb-dark.svg' },
+    },
   },
   {
     title: 'Sidebar',
