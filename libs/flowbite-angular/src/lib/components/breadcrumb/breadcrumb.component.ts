c44fdc247c6dd99c45d39cc0658cb962e9736fac
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2022-08-10 13:34:19 +0100

    fix: Breadcrumb was overengineered

--- libs/flowbite-angular/src/lib/components/breadcrumb/breadcrumb.component.ts
+++ libs/flowbite-angular/src/lib/components/breadcrumb/breadcrumb.component.ts
@@ -1,38 +1,13 @@
-import {
-  AfterViewInit,
-  Component,
-  ContentChildren,
-  Input,
-  QueryList,
-} from '@angular/core';
-import { BreadcrumbItemComponent } from './breadcrumb-item.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'flowbite-breadcrumb',
   template: `
-    <nav
-      [class]="
-        'flex ' +
-        (solidBackground
-          ? 'px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700'
-          : '')
-      "
-      aria-label="Breadcrumb"
-    >
-      <ol class="inline-flex items-center space-x-1 md:space-x-3">
+    <nav aria-label="Breadcrumb">
+      <ol class="flex items-center">
         <ng-content></ng-content>
       </ol>
     </nav>
   `,
 })
-export class BreadcrumbComponent implements AfterViewInit {
-  @ContentChildren(BreadcrumbItemComponent)
-  items?: QueryList<BreadcrumbItemComponent>;
-  @Input() solidBackground = false;
-
-  ngAfterViewInit() {
-    if (this.items?.first) {
-      this.items.first.setAsRoot();
-    }
-  }
-}
+export class BreadcrumbComponent {}
