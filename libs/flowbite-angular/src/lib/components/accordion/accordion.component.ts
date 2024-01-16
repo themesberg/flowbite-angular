4b01e61bec8c85fa4582e0ef97dbed316cef394f
MGREMY <126514209+MGREMY@users.noreply.github.com>
2024-01-16 21:29:13 +0100

    refactor(accordion): refactor accordion component template files

--- libs/flowbite-angular/src/lib/components/accordion/accordion.component.ts
+++ libs/flowbite-angular/src/lib/components/accordion/accordion.component.ts
@@ -5,12 +5,7 @@ import { NgClass } from '@angular/common';
   standalone: true,
   imports: [NgClass],
   selector: 'flowbite-accordion',
-  template: ` <div
-    class="divide-y divide-gray-200 overflow-hidden border-gray-200 dark:divide-gray-700 dark:border-gray-700"
-    [ngClass]="{ 'rounded-lg border': !flush, 'border-b': flush }"
-  >
-    <ng-content></ng-content>
-  </div>`,
+  templateUrl: './accordion.component.html',
 })
 export class AccordionComponent {
   @Input() flush?: boolean;
