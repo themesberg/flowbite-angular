34e980e3c1aa2b7a6bb71685455d16f381ae0c6e
MGREMY <126514209+MGREMY@users.noreply.github.com>
2024-01-21 21:55:50 +0100

    feat(docs): adding server_side_generation (#31)

--- apps/docs/src/app/app.config.ts
+++ apps/docs/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { appRoutes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [provideClientHydration(), provideRouter(appRoutes)],
 };
