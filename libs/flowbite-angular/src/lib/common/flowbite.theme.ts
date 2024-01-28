c1a97e5cf2942759e2aab6c24ceb23b9e8c7b0e1
MGREMY <126514209+MGREMY@users.noreply.github.com>
2024-01-28 22:32:41 +0100

    refactor(component/indicator)!: refactor styling code
    BREAKING CHANGES: `boolean` variables are now FlowbiteBoolean usable with `ènabled` or `disabled`

--- libs/flowbite-angular/src/lib/common/flowbite.theme.ts
+++ libs/flowbite-angular/src/lib/common/flowbite.theme.ts
@@ -67,12 +67,6 @@ export interface FlowbiteSizes {
   md: string;
   lg: string;
   xl: string;
-  '2xl': string;
-  '3xl': string;
-  '4xl': string;
-  '5xl': string;
-  '6xl': string;
-  '7xl': string;
 }
 
 export interface FlowbiteContentPositions {
--- libs/flowbite-angular/src/lib/components/indicators/indicators.component.ts
+++ libs/flowbite-angular/src/lib/components/indicators/indicators.component.ts
@@ -1,86 +1,13 @@
-import { Component, Input } from '@angular/core';
+import * as properties from './theme';
+import { BaseComponent } from '../base.component';
+import {
+  FlowbiteBoolean,
+  FlowbitePositions,
+  FlowbiteSizes,
+} from '../../common/flowbite.theme';
+
+import { Component, Input, OnInit } from '@angular/core';
 import { NgClass } from '@angular/common';
-
-const color:
-  | 'gray'
-  | 'dark'
-  | 'blue'
-  | 'green'
-  | 'red'
-  | 'purple'
-  | 'indigo'
-  | 'yellow'
-  | 'teal'
-  | 'none' = 'gray';
-
-const size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md';
-
-const placement:
-  | 'top-left'
-  | 'top-center'
-  | 'top-right'
-  | 'center-left'
-  | 'center'
-  | 'center-right'
-  | 'bottom-left'
-  | 'bottom-center'
-  | 'bottom-right'
-  | undefined = undefined;
-
-const colors = {
-  gray: 'bg-gray-200',
-  dark: 'bg-gray-900 dark:bg-gray-700',
-  blue: 'bg-blue-600',
-  green: 'bg-green-500',
-  red: 'bg-red-500',
-  purple: 'bg-purple-500',
-  indigo: 'bg-indigo-500',
-  yellow: 'bg-yellow-300',
-  teal: 'bg-teal-500',
-  none: '',
-};
-
-const sizes = {
-  xs: 'w-2 h-2',
-  sm: 'w-2.5 h-2.5',
-  md: 'w-3 h-3',
-  lg: 'w-3.5 h-3.5',
-  xl: 'w-6 h-6',
-};
-
-const placements = {
-  // top
-  'top-left': 'top-0 left-0',
-  'top-center': 'top-0 left-1/2 -translate-x-1/2',
-  'top-right': 'top-0 right-0',
-
-  // center
-  'center-left': 'top-1/2 -translate-y-1/2 left-0',
-  center: 'top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2',
-  'center-right': 'top-1/2 -translate-y-1/2 right-0',
-
-  // bottom
-  'bottom-left': 'bottom-0 left-0',
-  'bottom-center': 'bottom-0 left-1/2 -translate-x-1/2',
-  'bottom-right': 'bottom-0 right-0',
-};
-
-const offsets = {
-  // top
-  'top-left': '-translate-x-1/3 -translate-y-1/3',
-  'top-center': '-translate-y-1/3',
-  'top-right': 'translate-x-1/3 -translate-y-1/3',
-
-  // center
-  'center-left': '-translate-x-1/3',
-  center: '',
-  'center-right': 'translate-x-1/3',
-
-  // bottom
-  'bottom-left': '-translate-x-1/3 translate-y-1/3',
-  'bottom-center': 'translate-y-1/3',
-  'bottom-right': 'translate-x-1/3 translate-y-1/3',
-};
 
 @Component({
   standalone: true,
@@ -88,19 +15,30 @@ const offsets = {
   selector: 'flowbite-indicator',
   templateUrl: './indicators.component.html',
 })
-export class IndicatorComponent {
-  @Input() pill = false;
-  @Input() outline = false;
-  @Input() disabled = false;
-  @Input() offset = true;
-  @Input() rounded = false;
-  @Input() border = false;
-  @Input() color = color;
-  @Input() size = size;
-  @Input() placement = placement;
+export class IndicatorComponent extends BaseComponent implements OnInit {
+  @Input() pill: keyof FlowbiteBoolean = 'disabled';
+  @Input() outline: keyof FlowbiteBoolean = 'disabled';
+  @Input() disabled: keyof FlowbiteBoolean = 'disabled';
+  @Input() offset: keyof FlowbiteBoolean = 'enabled';
+  @Input() rounded: keyof FlowbiteBoolean = 'disabled';
+  @Input() border: keyof FlowbiteBoolean = 'disabled';
+  @Input() color: keyof properties.IndicatorColors = 'gray';
+  @Input() size: keyof FlowbiteSizes = 'md';
+  @Input() placement?: keyof FlowbitePositions;
+  @Input() customStyle: Partial<properties.IndicatorBaseTheme> = {};
 
-  colorClasses = colors;
-  sizeClasses = sizes;
-  placementClasses = placements;
-  offsetClasses = offsets;
+  ngOnInit(): void {
+    this.componentClass = properties.getClasses({
+      border: this.border,
+      color: this.color,
+      disabled: this.disabled,
+      offset: this.offset,
+      outline: this.outline,
+      pill: this.pill,
+      placement: this.placement,
+      rounded: this.rounded,
+      size: this.size,
+      customStyle: this.customStyle,
+    });
+  }
 }
