62b29c574ce2eb9838f22bb29090ee2e39411465
MGREMY <126514209+MGREMY@users.noreply.github.com>
2024-04-15 11:37:16 +0200

    Use SSR in docs app (#37)
    * Add SSR to documentation
    
    * feat(docs): add SSR
    
    * fix: rename json parameter
    
    * fix: vercel command
    
    * fix: fix api directory
    
    * fix: update api/index.js
    
    * fix: server.js to main.js
    
    * fix: fix
    
    * fix: fix
    
    * ci: update ci commands
    
    * feat: add client hydratation
    
    * fix: add start command to start ssr local

--- apps/docs/server.ts
+++ apps/docs/server.ts
@@ -10,7 +10,7 @@ import bootstrap from './src/main.server';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/docs-static');
+  const distFolder = join(process.cwd(), 'dist/docs-static/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html'))
     ? join(distFolder, 'index.original.html')
     : join(distFolder, 'index.html');
