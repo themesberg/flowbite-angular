33059488dc450196bf32ae97540c95cfe24b3672
MGREMY <mgremy.pro@gmail.com>
2024-10-06 18:58:39 +0200

    docs: add comments in service section for the documentation

--- libs/flowbite-angular/src/lib/services/state/dropdown.state.ts
+++ libs/flowbite-angular/src/lib/services/state/dropdown.state.ts
@@ -2,6 +2,11 @@ import { SignalStoreService } from '../signal-store.service';
 
 import { Injectable } from '@angular/core';
 
+/**
+ * Service that keep the state of an dropdown component.
+ *
+ * @see `DropdownComponent`
+ */
 @Injectable()
 export class DropdownStateService extends SignalStoreService<{
   isOpen: boolean;
--- libs/flowbite-angular/src/lib/services/state/navbar.state.ts
+++ libs/flowbite-angular/src/lib/services/state/navbar.state.ts
@@ -2,6 +2,11 @@ import { SignalStoreService } from '../signal-store.service';
 
 import { Injectable } from '@angular/core';
 
+/**
+ * Service that keep the state of an navbar component.
+ *
+ * @see `NavbarComponent`
+ */
 @Injectable()
 export class NavbarStateService extends SignalStoreService<{
   isOpen: boolean;
