d9d3ed6b1d15f9c8f640e67f37af7014441a89da
Bence Lovász <lovasz.b99@gmail.com>
2024-10-09 00:06:11 +0200

    fix(sidebar): inherit color from itemgroup/menu (it not provided)

--- libs/flowbite-angular/src/lib/components/sidebar/sidebar-item.component.ts
+++ libs/flowbite-angular/src/lib/components/sidebar/sidebar-item.component.ts
@@ -12,7 +12,14 @@ import { SidebarMenuComponent } from './sidebar-menu.component';
 import type { SidebarColors } from './sidebar.theme';
 
 import { NgClass, NgIf } from '@angular/common';
-import { Component, HostListener, inject, input, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  HostListener,
+  inject,
+  input,
+  signal
+} from '@angular/core';
 
 @Component({
   standalone: true,
@@ -45,25 +52,44 @@ export class SidebarItemComponent extends BaseComponent {
   public readonly flowbiteRouterLink = inject(FlowbiteRouterLinkDirective);
   public readonly flowbiteRouterLinkActive = inject(FlowbiteRouterLinkActiveDirective);
   public readonly themeService = inject(SidebarItemThemeService);
-  public readonly sidebarItemGroupComponent = inject<SidebarItemGroupComponent | undefined>(SidebarItemGroupComponent);
+  public readonly sidebarItemGroupComponent = inject<SidebarItemGroupComponent | undefined>(SidebarItemGroupComponent, { optional: true });
   public readonly sidebarMenuComponent = inject<SidebarMenuComponent | undefined>(SidebarMenuComponent);
 
   public override contentClasses = signal<SidebarItemClass>(createClass({ rootClass: '', sidebarIconClass: '' }));
 
   //#region properties
   public icon = input<string | undefined>(undefined);
-  public color = input<keyof SidebarColors>(
-    (this.sidebarItemGroupComponent?.sidebarMenuComponent || this.sidebarMenuComponent)!.color(),
-  );
+  public color = input<keyof SidebarColors>();
   public label = input<string | undefined>(undefined);
   public customStyle = input<DeepPartial<SidebarItemTheme>>({});
+
+  // With a computed, it's possible to dynamically
+  // change the color (even if the SidebarMenuComponent's
+  // or SidebarItemGroupComponent's color has changed)
+  private realColor = computed(() => {
+    // If the color is set directly on the
+    // SidebarItemComponent we just return it as is
+    if (this.color() !== undefined) {
+      return this.color() as keyof SidebarColors;
+    }
+
+    // If the component is inside a SidebarItemGroupComponent
+    // we return it's color
+    if (this.sidebarItemGroupComponent) {
+      return this.sidebarItemGroupComponent.color();
+    }
+
+    // Since a SidebarMenuComponent is required, we return
+    // its color if none of the above conditions are met
+    return this.sidebarMenuComponent?.color() ?? 'primary';
+  });
   //#endregion
 
   //#region BaseComponent implementation
   public override fetchClass(): void {
     const propertyClass = this.themeService.getClasses({
       icon: this.icon(),
-      color: this.color(),
+      color: this.realColor(),
       label: this.label(),
       customStyle: this.customStyle(),
     });
@@ -72,8 +98,8 @@ export class SidebarItemComponent extends BaseComponent {
   }
 
   public override verify(): void {
-    if (this.sidebarMenuComponent === undefined && this.sidebarItemGroupComponent === undefined) {
-      throw new Error('No SidebarMenuComponent/SidebarItemGroupComponent available');
+    if (this.sidebarMenuComponent === undefined) {
+      throw new Error('No SidebarMenuComponent available');
     }
   }
   //#endregion
