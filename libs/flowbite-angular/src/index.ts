b4dd331ca71796f6320106d526d1dda86b57df10
MGREMY <mgremy.pro@gmail.com>
2024-10-16 16:39:13 +0200

    chore: add public-api.ts file, preparing for segmentation with ng-packagr

--- libs/flowbite-angular/src/index.ts
+++ libs/flowbite-angular/src/index.ts
@@ -1,6 +1,1 @@
-export * from './lib/common';
-export * from './lib/components';
-export * from './lib/pipes';
-export * from './lib/services';
-export * from './lib/utils';
-export * from './lib/directives';
+export * from './public-api';
