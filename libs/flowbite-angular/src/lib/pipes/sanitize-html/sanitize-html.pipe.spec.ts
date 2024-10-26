ab42449810370370786885fd529880c847ec93ff
MGREMY <miguel.gremy72+pro@gmail.com>
2024-10-26 19:00:35 +0200

    test: fix tests

--- libs/flowbite-angular/src/lib/pipes/sanitize-html/sanitize-html.pipe.spec.ts
+++ libs/flowbite-angular/src/lib/pipes/sanitize-html/sanitize-html.pipe.spec.ts
@@ -1,9 +1,17 @@
 import { SanitizeHtmlPipe } from '.';
-import { ɵDomSanitizerImpl } from '@angular/platform-browser';
+
+import { TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('SanitizeHtmlPipe', () => {
+  let pipe: SanitizeHtmlPipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ providers: [DomSanitizer, SanitizeHtmlPipe] });
+    pipe = TestBed.inject(SanitizeHtmlPipe);
+  });
+
   it('create an instance', () => {
-    const pipe = new SanitizeHtmlPipe(new ɵDomSanitizerImpl(document));
     expect(pipe).toBeTruthy();
   });
 });
