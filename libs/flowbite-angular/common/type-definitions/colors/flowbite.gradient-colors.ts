7b319f4a2b0a6d04fd41daa00fa90173d795f9f2
MGREMY <miguel.gremy72+pro@gmail.com>
2024-10-28 09:48:27 +0100

    docs(common): added JSDoc for API documentation autogeneration

--- libs/flowbite-angular/common/type-definitions/colors/flowbite.gradient-colors.ts
+++ libs/flowbite-angular/common/type-definitions/colors/flowbite.gradient-colors.ts
@@ -3,7 +3,6 @@ import type { FlowbiteColors } from './flowbite.colors';
 /**
  * Base definition of gradient colors used in flowbite-angular.
  */
-
 export interface FlowbiteGradientColors extends Omit<FlowbiteColors, 'warning'> {
   cyan: string;
   lime: string;
