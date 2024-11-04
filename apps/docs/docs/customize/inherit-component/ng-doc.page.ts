266eadb5b2b36ab51bdc2a741ed865d402df0bf9
MGREMY <miguel.gremy72+pro@gmail.com>
2024-11-04 14:43:42 +0100

    docs: add inherit component documentation

---
+++ apps/docs/docs/customize/inherit-component/ng-doc.page.ts
@@ -0,0 +1,15 @@
+import CustomizeCategory from '../ng-doc.category';
+
+import type { NgDocPage } from '@ng-doc/core';
+
+/**
+ * Customize flowbite-angular's component style by inheriting component.
+ */
+const OverrideBaseStyle: NgDocPage = {
+  title: 'Inherit component',
+  mdFile: './index.md',
+  category: CustomizeCategory,
+  order: 4,
+};
+
+export default OverrideBaseStyle;
