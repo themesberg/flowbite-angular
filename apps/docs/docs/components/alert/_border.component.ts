55045055a6b36e56748f18a16831e3e7116450a1
MGREMY <miguel.gremy72+pro@gmail.com>
2024-11-04 16:57:04 +0100

    docs(alert): add spacing in examples

--- apps/docs/docs/components/alert/_border.component.ts
+++ apps/docs/docs/components/alert/_border.component.ts
@@ -9,43 +9,40 @@ import { Component } from '@angular/core';
   template: `
     <flowbite-alert
       [hasBorder]="true"
-      color="primary"
-      class="w-full">
+      color="primary">
       <span class="font-medium">Primary alert !</span> Change a few things up and try submitting
       again.
     </flowbite-alert>
     <flowbite-alert
       [hasBorder]="true"
-      color="dark"
-      class="w-full">
+      color="dark">
       <span class="font-medium">Dark alert !</span> Change a few things up and try submitting again.
     </flowbite-alert>
     <flowbite-alert
       [hasBorder]="true"
-      color="blue"
-      class="w-full">
+      color="blue">
       <span class="font-medium">blue alert !</span> Change a few things up and try submitting again.
     </flowbite-alert>
     <flowbite-alert
       [hasBorder]="true"
-      color="red"
-      class="w-full">
+      color="red">
       <span class="font-medium">red alert !</span> Change a few things up and try submitting again.
     </flowbite-alert>
     <flowbite-alert
       [hasBorder]="true"
-      color="green"
-      class="w-full">
+      color="green">
       <span class="font-medium">green alert !</span> Change a few things up and try submitting
       again.
     </flowbite-alert>
     <flowbite-alert
       [hasBorder]="true"
-      color="yellow"
-      class="w-full">
+      color="yellow">
       <span class="font-medium">yellow alert !</span> Change a few things up and try submitting
       again.
     </flowbite-alert>
   `,
+  host: {
+    class: 'flex flex wrap flex-col gap-3',
+  },
 })
 export class FlowbiteBorderComponent {}
--- apps/docs/docs/components/alert/_default.component.ts
+++ apps/docs/docs/components/alert/_default.component.ts
@@ -7,39 +7,30 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [AlertComponent],
   template: `
-    <flowbite-alert
-      color="primary"
-      class="w-full">
+    <flowbite-alert color="primary">
       <span class="font-medium">primary alert !</span> Change a few things up and try submitting
       again.
     </flowbite-alert>
-    <flowbite-alert
-      color="dark"
-      class="w-full">
+    <flowbite-alert color="dark">
       <span class="font-medium">Dark alert !</span> Change a few things up and try submitting again.
     </flowbite-alert>
-    <flowbite-alert
-      color="blue"
-      class="w-full">
+    <flowbite-alert color="blue">
       <span class="font-medium">blue alert !</span> Change a few things up and try submitting again.
     </flowbite-alert>
-    <flowbite-alert
-      color="red"
-      class="w-full">
+    <flowbite-alert color="red">
       <span class="font-medium">red alert !</span> Change a few things up and try submitting again.
     </flowbite-alert>
-    <flowbite-alert
-      color="green"
-      class="w-full">
+    <flowbite-alert color="green">
       <span class="font-medium">green alert !</span> Change a few things up and try submitting
       again.
     </flowbite-alert>
-    <flowbite-alert
-      color="yellow"
-      class="w-full">
+    <flowbite-alert color="yellow">
       <span class="font-medium">yellow alert !</span> Change a few things up and try submitting
       again.
     </flowbite-alert>
   `,
+  host: {
+    class: 'flex flex wrap flex-col gap-3',
+  },
 })
 export class FlowbiteDefaultComponent {}
