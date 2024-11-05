00cd951b4f429207b6b29de8a36313751a543164
MGREMY <miguel.gremy72+pro@gmail.com>
2024-11-05 16:26:31 +0100

    docs(landing): fix getting started routerLink

--- apps/docs/src/app/pages/landing/landing.component.ts
+++ apps/docs/src/app/pages/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { ButtonComponent } from 'flowbite-angular/button';
 import { IconComponent } from 'flowbite-angular/icon';
+import { FlowbiteRouterLinkDirective } from 'flowbite-angular/router-link';
 
 import { Component, inject } from '@angular/core';
 import { NgDocThemeService } from '@ng-doc/app/services/theme';
@@ -7,7 +8,7 @@ import { NgDocThemeService } from '@ng-doc/app/services/theme';
 @Component({
   selector: 'flowbite-landing',
   standalone: true,
-  imports: [IconComponent, ButtonComponent],
+  imports: [IconComponent, ButtonComponent, FlowbiteRouterLinkDirective],
   templateUrl: './landing.component.html',
 })
 export class LandingComponent {
