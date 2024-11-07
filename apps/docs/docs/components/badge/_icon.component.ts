de7eb48fcb58f3cfb99d240737edbbbc19432d59
MGREMY <miguel.gremy72+pro@gmail.com>
2024-11-07 11:59:19 +0100

    docs: move badge template into files

--- apps/docs/docs/components/badge/_icon.component.ts
+++ apps/docs/docs/components/badge/_icon.component.ts
@@ -7,20 +7,7 @@ import { Component } from '@angular/core';
   selector: 'flowbite-demo-badge-icon',
   standalone: true,
   imports: [BadgeComponent, IconComponent],
-  template: `
-    <flowbite-badge>
-      <flowbite-icon
-        svgIcon="outline:check"
-        class="h-3 w-3" />
-      2 minutes ago
-    </flowbite-badge>
-    <flowbite-badge color="primary">
-      <flowbite-icon
-        svgIcon="outline:clock"
-        class="h-3 w-3" />
-      3 days ago
-    </flowbite-badge>
-  `,
+  templateUrl: './_icon.component.html',
   host: {
     class: 'flex flex wrap flex-row gap-3',
   },
