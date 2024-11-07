8ef015a92077e2145e68f46bf6af41988862ec15
MGREMY <miguel.gremy72+pro@gmail.com>
2024-11-07 14:15:57 +0100

    docs: move modal template into files

--- apps/docs/docs/components/modal/_dismissable.component.ts
+++ apps/docs/docs/components/modal/_dismissable.component.ts
@@ -18,35 +18,7 @@ import { Component } from '@angular/core';
     ModalFooterComponent,
     ButtonComponent,
   ],
-  template: `
-    <flowbite-button (click)="dismissableModal.open()">Open modal</flowbite-button>
-    <flowbite-modal
-      #dismissableModal
-      [isDismissable]="true">
-      <flowbite-modal-header>Terms of Service</flowbite-modal-header>
-      <flowbite-modal-body>
-        <p class="mb-2 text-gray-500 dark:text-gray-400">
-          With less than a month to go before the European Union enacts new consumer privacy laws
-          for its citizens, companies around the world are updating their terms of service
-          agreements to comply.
-        </p>
-        <p class="text-gray-500 dark:text-gray-400">
-          The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May
-          25 and is meant to ensure a common set of data rights in the European Union. It requires
-          organizations to notify users as soon as possible of high-risk data breaches that could
-          personally affect them.
-        </p>
-      </flowbite-modal-body>
-      <flowbite-modal-footer>
-        <flowbite-button (click)="dismissableModal.close()">I accept</flowbite-button>
-        <flowbite-button
-          (click)="dismissableModal.close()"
-          color="light">
-          I decline
-        </flowbite-button>
-      </flowbite-modal-footer>
-    </flowbite-modal>
-  `,
+  templateUrl: '_dismissable.component.html',
   host: {
     class: 'flex flex wrap flex-row gap-3',
   },
