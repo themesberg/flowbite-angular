463b2556547e18deea8f2f84df0cf963a44462e0
MGREMY <miguel.gremy72+pro@gmail.com>
2024-12-05 10:36:35 +0100

    feat(theme): add signal based accessor to current theme #82

--- libs/flowbite-angular/theme/flowbite-theme.directive.ts
+++ libs/flowbite-angular/theme/flowbite-theme.directive.ts
@@ -1,7 +1,7 @@
 import type { FlowbiteTheme } from 'flowbite-angular';
 
 import type { OnInit } from '@angular/core';
-import { afterNextRender, Directive, inject, Injector } from '@angular/core';
+import { afterNextRender, Directive, inject, Injector, signal, untracked } from '@angular/core';
 
 /**
  * Use to give a clean API on the current application theme
@@ -14,32 +14,39 @@ export class FlowbiteThemeDirective implements OnInit {
   private readonly injector = inject(Injector);
 
   private readonly localStorageKey = 'color-theme';
+  private readonly _currentTheme = signal<FlowbiteTheme>('light');
 
   public ngOnInit(): void {
     afterNextRender(
       () => {
-        this.toggleTheme(this.getTheme());
+        this.toggleTheme(this.getLocalStorageTheme());
       },
       { injector: this.injector }
     );
   }
 
   /**
+   * Get the application theme based on `signals`
+   */
+  public readonly currentTheme = this._currentTheme.asReadonly();
+
+  /**
    * Get theme from the `localStorage`
+   *
    * @returns The current theme saved in the `localStorage` with the key `color-theme`
    */
-  public getTheme(): FlowbiteTheme {
+  public getLocalStorageTheme(): FlowbiteTheme {
     return localStorage.getItem('color-theme') === 'dark' ? 'dark' : 'light';
   }
 
   /**
-   * Toggle the theme saced in the `localStorage`
+   * Toggle the theme saved in the `localStorage` and available in `currentTheme`
    *
    * @param theme If provided, force the theme instead of toggling it between light and dark mode
    */
   public toggleTheme(theme?: FlowbiteTheme): void {
     if (!theme) {
-      const tmpTheme = this.getTheme();
+      const tmpTheme = untracked(() => this.currentTheme());
 
       if (tmpTheme === 'dark') theme = 'light';
       else theme = 'dark';
@@ -55,6 +62,7 @@ export class FlowbiteThemeDirective implements OnInit {
    */
   public setTheme(theme: FlowbiteTheme): void {
     localStorage.setItem(this.localStorageKey, theme);
+    this._currentTheme.set(theme);
 
     theme === 'dark'
       ? document.documentElement.classList.add('dark')
