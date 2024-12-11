e6482dfb5e4f49f0738c93deb7cd1e918abfa509
Bence Lovász <lovasz.b99@gmail.com>
2024-12-11 22:24:26 +0100

    feat(pagination): implement basics based on flowbite (wip)

---
+++ libs/flowbite-angular/pagination/index.ts
@@ -0,0 +1,7 @@
+export { PaginationComponent } from './pagination.component';
+export type { PaginationProperties, PaginationClass, PaginationTheme } from './pagination.theme';
+export { paginationTheme } from './pagination.theme';
+export {
+  PaginationThemeService,
+  FLOWBITE_PAGINATION_THEME_TOKEN,
+} from './pagination.theme.service';
---
+++ libs/flowbite-angular/pagination/pagination.theme.service.ts
@@ -0,0 +1,38 @@
+import type { PaginationClass, PaginationProperties, PaginationTheme } from './pagination.theme';
+
+import type { FlowbiteThemeService } from 'flowbite-angular';
+import { mergeTheme } from 'flowbite-angular/utils';
+
+import { inject, Injectable, InjectionToken } from '@angular/core';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * `InjectionToken` used to import `PaginationTheme` value
+ *
+ * @example
+ * ```
+ * var theme = inject(FLOWBITE_PAGINATION_THEME)
+ * ```
+ */
+export const FLOWBITE_PAGINATION_THEME_TOKEN = new InjectionToken<PaginationTheme>(
+  'FLOWBITE_PAGINATION_THEME'
+);
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaginationThemeService implements FlowbiteThemeService<PaginationProperties> {
+  private readonly baseTheme = inject(FLOWBITE_PAGINATION_THEME_TOKEN);
+
+  public getClasses(properties: PaginationProperties): PaginationClass {
+    const theme: PaginationTheme = mergeTheme(this.baseTheme, properties.customStyle);
+
+    const output: PaginationClass = {
+      rootClass: twMerge(theme.root.base),
+      navigationClass: twMerge(theme.navigation.base),
+      listItemClass: twMerge(theme.listItem.base),
+    };
+
+    return output;
+  }
+}
