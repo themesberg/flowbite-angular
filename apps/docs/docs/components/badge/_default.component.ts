adfebeb62c6c778d9fa66404ab7c802324d41760
MGREMY <miguel.gremy72+pro@gmail.com>
2024-12-19 11:31:13 +0100

    docs(badge): fix type in class

--- apps/docs/docs/components/badge/_default.component.ts
+++ apps/docs/docs/components/badge/_default.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   imports: [BadgeComponent],
   templateUrl: './_default.component.html',
   host: {
-    class: 'flex flex wrap flex-row gap-3',
+    class: 'flex flex-wrap flex-row gap-3',
   },
 })
 export class FlowbiteDefaultComponent {}
