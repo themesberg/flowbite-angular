e6ac3eda3c91f8191ce0bd2693d2811f0a48610e
MGREMY <miguel.gremy72+pro@gmail.com>
2024-12-30 15:22:12 +0100

    docs: add standard configuration doc section and update indicator style

---
+++ apps/docs/docs/customize/standard-configuration/ng-doc.page.ts
@@ -0,0 +1,17 @@
+import CustomizeCategory from '../ng-doc.category';
+
+import type { NgDocPage } from '@ng-doc/core';
+
+/**
+ * Defining a standardized way to create themes across all components of flowbite-angular.
+ *
+ * @status:info NEW
+ */
+const StandardConfiguration: NgDocPage = {
+  title: 'Standard configuration',
+  mdFile: './index.md',
+  category: CustomizeCategory,
+  order: 7,
+};
+
+export default StandardConfiguration;
