2174be0047e79c0a713747e2d791c65f01e74cb3
MGREMY <miguel.gremy72+pro@gmail.com>
2025-04-11 13:23:55 +0200

    refactor(button): adapt outline style

--- libs/flowbite-angular/button/src/button/theme.ts
+++ libs/flowbite-angular/button/src/button/theme.ts
@@ -29,7 +29,7 @@ export interface FlowbiteButtonHostTheme {
 
 export const flowbiteButtonTheme: FlowbiteButtonTheme = createTheme({
   host: {
-    base: 'relative border font-medium',
+    base: 'relative cursor-pointer border font-medium',
     focus: 'data-focus:ring-4 data-focus:outline-none',
     disabled: 'data-disabled:cursor-not-allowed data-disabled:opacity-50',
     size: {
@@ -48,7 +48,7 @@ export const flowbiteButtonTheme: FlowbiteButtonTheme = createTheme({
         'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-800 dark:focus:bg-primary-900 border-transparent text-white',
       dark: 'border-transparent bg-gray-800 text-white hover:bg-gray-900 focus:ring-gray-300 dark:bg-gray-800 dark:focus:ring-gray-800',
       light:
-        'border-gray-300 bg-white text-gray-900 hover:bg-gray-100 focus:ring-blue-300 dark:bg-gray-600 dark:text-white dark:focus:ring-gray-700',
+        'border-gray-300 bg-white text-black hover:bg-gray-100 focus:ring-blue-300 dark:bg-gray-600 dark:text-white dark:focus:ring-gray-700',
       green:
         'border-transparent bg-green-700 text-white hover:bg-green-800 focus:ring-green-300 dark:bg-green-600 dark:focus:ring-green-800',
       red: 'border-transparent bg-red-700 text-white hover:bg-red-800 focus:ring-red-300 dark:bg-red-600 dark:focus:ring-red-900',
@@ -58,17 +58,14 @@ export const flowbiteButtonTheme: FlowbiteButtonTheme = createTheme({
         'border-transparent bg-purple-700 text-white hover:bg-purple-800 focus:ring-purple-300 dark:bg-purple-600 dark:focus:ring-purple-900',
     },
     colorOutline: {
-      primary: 'border-primary-800 dark:border-primary-800 text-white',
-      dark: 'border-transparent bg-gray-800 text-white hover:bg-gray-900 focus:ring-gray-300 dark:bg-gray-800 dark:focus:ring-gray-800',
-      light:
-        'border-gray-300 bg-white text-gray-900 hover:bg-gray-100 focus:ring-blue-300 dark:bg-gray-600 dark:text-white dark:focus:ring-gray-700',
-      green:
-        'border-transparent bg-green-700 text-white hover:bg-green-800 focus:ring-green-300 dark:bg-green-600 dark:focus:ring-green-800',
-      red: 'border-transparent bg-red-700 text-white hover:bg-red-800 focus:ring-red-300 dark:bg-red-600 dark:focus:ring-red-900',
-      yellow:
-        'border-transparent bg-yellow-400 text-white hover:bg-yellow-500 focus:ring-yellow-300 dark:bg-yellow-400 dark:focus:ring-yellow-900',
-      purple:
-        'border-transparent bg-purple-700 text-white hover:bg-purple-800 focus:ring-purple-300 dark:bg-purple-600 dark:focus:ring-purple-900',
+      primary:
+        'border-primary-800 text-primary-800 data-hover:bg-primary-800 data-hover:text-white',
+      dark: 'border-gray-800 text-gray-800 data-hover:bg-gray-800 data-hover:text-white',
+      light: 'border-gray-300 text-black data-hover:bg-gray-300 data-hover:text-black',
+      green: 'border-green-700 text-green-700 data-hover:bg-green-700 data-hover:text-white',
+      red: 'border-red-700 text-red-700 data-hover:bg-red-700 data-hover:text-white',
+      yellow: 'border-yellow-400 text-yellow-400 data-hover:bg-yellow-400 data-hover:text-white',
+      purple: 'border-purple-700 text-purple-700 data-hover:bg-purple-700 data-hover:text-white',
     },
   },
 });
