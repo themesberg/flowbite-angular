d1d23de2c8e5b707342622e8f7b0756542a5849d
MGREMY <miguel.gremy72+pro@gmail.com>
2025-04-11 17:46:55 +0200

    refactor: move to css tailwind config

--- apps/storybook/storybook-tailwind.config.ts
+++ apps/storybook/storybook-tailwind.config.ts
@@ -1,6 +1,4 @@
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import sharedTailwindConfig from '../../libs/flowbite-angular/tailwind.config';
-
 import { createGlobPatternsForDependencies } from '@nx/angular/tailwind';
 
 const content = createGlobPatternsForDependencies(__dirname, '**/*.{ts,html}');
@@ -8,6 +6,5 @@ const content = createGlobPatternsForDependencies(__dirname, '**/*.{ts,html}');
 /** @type {import('tailwindcss').Config} */
 export default {
   content,
-  presets: [{ ...sharedTailwindConfig }, content],
-  darkMode: ['class', '[data-mode="dark"]'],
+  presets: [content],
 };
