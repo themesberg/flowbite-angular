40be69ac0c0fa96bd79c96e3cefecc09fcad92d5
MGREMY <miguel.gremy72+pro@gmail.com>
2025-07-04 01:01:07 +0200

    docs: fix badge icons

--- libs/flowbite-angular/alert/src/alert/theme.ts
+++ libs/flowbite-angular/alert/src/alert/theme.ts
@@ -25,27 +25,27 @@ export const flowbiteAlertTheme: FlowbiteAlertTheme = createTheme({
     color: {
       default: {
         light: 'border-gray-200 bg-gray-50',
-        dark: 'dark:border-gray-700 dark:bg-gray-800',
+        dark: 'dark:border-gray-700 dark:bg-gray-950',
       },
       info: {
         light: 'border-blue-200 bg-blue-50',
-        dark: 'dark:border-blue-700 dark:bg-blue-800',
+        dark: 'dark:border-blue-700 dark:bg-blue-950',
       },
       failure: {
         light: 'border-red-200 bg-red-50',
-        dark: 'dark:border-red-700 dark:bg-red-800',
+        dark: 'dark:border-red-700 dark:bg-red-950',
       },
       success: {
         light: 'border-green-200 bg-green-50',
-        dark: 'dark:border-green-700 dark:bg-green-800',
+        dark: 'dark:border-green-700 dark:bg-green-950',
       },
       warning: {
         light: 'border-yellow-200 bg-yellow-50',
-        dark: 'dark:border-yellow-700 dark:bg-yellow-800',
+        dark: 'dark:border-yellow-700 dark:bg-yellow-950',
       },
       primary: {
         light: 'bg-primary-50 border-primary-200',
-        dark: 'dark:bg-primary-800 dark:border-primary-700',
+        dark: 'dark:bg-primary-950 dark:border-primary-700',
       },
     },
     border: {
