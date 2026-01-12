b07a588c34643a0c25e19c2936048c5d2e661bbe
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-01-13 00:12:41 +0100

    chore(release): update to angular 21

--- apps/docs/src/main.server.ts
+++ apps/docs/src/main.server.ts
@@ -1,6 +1,7 @@
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
+import { provideZoneChangeDetection } from '@angular/core';
 import type { BootstrapContext } from '@angular/platform-browser';
 import { bootstrapApplication } from '@angular/platform-browser';
 
