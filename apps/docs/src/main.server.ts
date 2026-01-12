9579f117a5f88e519a682d5ace3f88b8cf1fd73e
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-01-13 00:02:22 +0100

    chore(release): update to angular 21

--- apps/docs/src/main.server.ts
+++ apps/docs/src/main.server.ts
@@ -1,8 +1,14 @@
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
+import type { BootstrapContext } from '@angular/platform-browser';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = (context: BootstrapContext) =>
+  bootstrapApplication(
+    AppComponent,
+    { ...config, providers: [provideZoneChangeDetection(), ...config.providers] },
+    context
+  );
 
 export default bootstrap;
--- apps/docs/src/main.ts
+++ apps/docs/src/main.ts
@@ -2,11 +2,14 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { environment } from './environments/environment';
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig).catch((e) => console.log(e));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((e) => console.log(e));
